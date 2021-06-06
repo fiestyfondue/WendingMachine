3fed6caec83e86bb7d819cdc57c6fd14c7b0f97d
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-06-06 10:02:30 +0530

    [Harsh]:-Created a simple Wending machine with no user inputs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Wending_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WendingMachine WM= new WendingMachine();
+
+            WM.depositCoin(199);
+            WM.GetDrink();
+            //WM.Refund();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WendingMachine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wending_Machine
+{
+    class WendingMachine
+    {
+        private int Cost_of_Drink=75;
+        private int depositedAmount;
+        
+
+       
+        public WendingMachine()                      //Constructor
+        {
+            depositedAmount += 0;
+        }
+
+        public void depositCoin(int coinAmount)
+        {
+            depositedAmount = coinAmount;
+        }
+        public void GetDrink()
+        {
+            if(depositedAmount>=Cost_of_Drink)
+            {
+                Console.WriteLine("Your change is {0} rupees",depositedAmount - Cost_of_Drink);
+                depositCoin(0);
+            }
+            else
+            {
+                Console.WriteLine("Insert more coins");
+            }
+        }
+
+        public void Refund()
+        {
+            Console.WriteLine("You are refunded {0}", depositedAmount);
+            depositedAmount=0;
+        }
+    }
+}
